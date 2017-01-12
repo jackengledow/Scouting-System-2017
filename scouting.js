90c5610674f4eb567e2484f539451b6678b5fcde
Ethan <ecounen74@gmail.com>
2017-01-12 17:57:04 -0500

    made some more good changes to auton

--- scouting.js
+++ scouting.js
@@ -9,3 +9,14 @@ var countHighMakeM=function () {
 	i--;
 	console.log(i);
 }
+var o=0;
+var countHighMakeP=function (){
+	o++; 
+	console.log(o);
+	return o;
+}
+
+var countHighMakeM=function () {
+	o--;
+	console.log(o);
+}
