b31f26066dce16d561e6dd13a1c890114bc906a5
Austin S. Hartman <austinhartman@ccs.k12.in.us>
2017-01-30 16:05:47 -0500

    working on heatmap

--- scouting.js
+++ scouting.js
@@ -19,6 +19,8 @@ var bigLoadsTeleop = [""];
 var bigCountAuton = 0;
 var bigCountTeleop = 0;
 var canvas = document.getElementById('cnv');
+var gridCounterMake = [[0],[0],[0],[0],[0]];
+var gridCounterMiss = [[0],[0],[0],[0],[0]];
 
 var countHighMakeTeleop=function (change){
 	makeHighTeleop+=change;
@@ -114,4 +116,47 @@ var dumpPercentTeleop = function(percent){
 		bigCountTeleop++;
 		console.log("Big Teleop: " + bigLoadsTeleop);
 	}
+}
+
+var clickZone = function (x,y,makemiss){
+	if(x>=15 && x<89){
+		index_x = 0;
+	}
+	if(x>=89 && x<163){
+		index_x = 1;
+	}
+	if(x>=163 && x<237){
+		index_x = 2;
+	}
+	if(x>=237 && x<311){
+		index_x = 3;
+	}
+	if(x>=311 && x<384){
+		index_x = 4;
+	}
+	
+	
+	if(y>=65 && y<145){
+		index_y = 0;
+	}
+	if(y>=145 && y<226){
+		index_y = 1;
+	}
+	if(y>=226 && y<305){
+		index_y = 2;
+	}
+	if(y>=305 && y<386){
+		index_y = 3;
+	}
+	if(y>=386 && y<464){
+		index_y = 4;
+	}
+	
+	if(makemiss = "Make"){
+		gridCounterMake[index_x,index_y] += 1;
+	}
+	if(makemiss = "Miss"){
+		gridCounterMiss[index_x,index_y] += 1;
+	}
+	console.log("When shooting from spot " + index_x + "," + index_y + ". Make: " + gridCounterMake[index_x, index_y] + ". Miss: " + gridCounterMiss[index_x, index_y] + ". Percent: " + gridCounterMake[index_x, index_y]/(gridCounterMake[index_x, index_y]+gridCounterMiss[index_x, index_y])+ "%.");
 }
