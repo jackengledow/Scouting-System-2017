e9ff1faa661ae966cd877444a1ca34e7688a3353
Alec C. Booth <alecbooth@ccs.k12.in.us>
2017-02-16 16:10:43 -0500

    failed

--- scouting.js
+++ scouting.js
@@ -332,20 +332,6 @@ var clearArray = function(){
 	}
 }
 
-var moveSubmitting = function() {
-	 jQuery('.tabs .tab-links a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
-
-        // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
-
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
-
-        e.preventDefault();
-		
-    });
-}
 var __slice = Array.prototype.slice;
 (function($) {
   var Sketch;
